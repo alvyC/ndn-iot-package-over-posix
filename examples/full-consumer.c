2018119e1be3b34daeafbadef837a2ece917fe26
Saurab Dulal <sdulal@memphis.edu>
2019-09-07 18:50:25 -0400

    added some codes

--- examples/full-consumer.c
+++ examples/full-consumer.c
@@ -1,7 +1,28 @@
-#include "ndn-lite/psync-fconsumer/full-consumer.c"
+#include "ndn-lite/psync-fconsumer/full-consumer.h"
+#include "../ndn-lite/face/dummy-face.h"
 
+#include <stdio.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <ndn-lite.h>
+#include "ndn-lite/encode/name.h"
+#include "ndn-lite/encode/data.h"
+#include "ndn-lite/encode/interest.h"
+#include <time.h>
+#include <stdlib.h>
+
+void onUpdate() {;}
 
 int main() {
 
-    return 0;
+full_consumer* c;
+char const arr[] = "ABCD";
+ndn_udp_face_t *udp_face;
+time_t time;
+// void (*ptr)() = &onUpdate; 
+
+init_full_consumer(udp_face, arr, &onUpdate);
+
 }
+
